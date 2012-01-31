cf92496aa509ebe4b177c442482eb853114b1235
Shane McDonald <anatomyofashane@gmail.com>
2012-01-31 11:45:32 -0500

    more tests

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe ListingsController do
   render_views
 
-  describe "GET 'listing'" do
+  describe "GET 'show'" do
+
+    before(:each) do  
+      @listing = Factory(:listing)
+    end 
+
     it "should be successful" do
       get 'index'
       response.should be_success
@@ -17,8 +22,12 @@ describe ListingsController do
 
   describe "GET 'edit'" do
 
+    before(:each) do  
+      @listing = Factory(:listing)
+    end 
+
     it "should get the edit page for a listing" do
-      
+      get :edit, :id => @listing
     end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,16 @@
-Factory.define :user do |user|
-  user.name       "Shane McDonald"
-  user.uid        100002897004783
+FactoryGirl.define do
+
+  factory :user do
+    name  "Shane McDonald"
+    uid   100002897004783
+  end
+
+  factory :listing do
+    name  "Beach house"
+    latitude 32.7153292
+    longitude -117.1572551
+    address "San Diego CA"
+    city "San Diego"
+    state "CA"
+  end
 end
