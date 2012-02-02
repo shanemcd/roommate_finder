90bba7502bc5ce9d97347617b36db8b69b9062db
Shane McDonald <anatomyofashane@gmail.com>
2012-02-02 17:11:07 -0500

    added edit view for the user and some tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,21 @@ class UsersController < ApplicationController
       @title = @user.name
       @listings = @user.listings.all
     end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+        if @user.update_attributes!(params[:user])
+          flash[:notice] = "Successfully updated user account."
+          redirect_to @user
+        else
+          render :action => 'edit'
+        end
+      rescue Exception => e #rescue the right exception, when you remember what it was
+      flash[:error] = e.message
+      redirect_to edit_user_path(@user)
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -34,4 +34,55 @@ describe UsersController do
       response.should have_selector("img", :class => "user_img")
     end
   end
+
+  describe "GET 'edit'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should get a user edit page" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+  end
+
+  describe "POST 'update'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+      
+      it "should not update a user" do
+        lambda do
+          post :update, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      # it "should have the right title" do
+      #   post :create, :user => @attr
+      #   response.should have_selector("title", :content => "Sign up")
+      # end
+      
+      it "should redirect to the user edit page" do
+        post :update, :user => @attr
+        response.should redirect_to(edit_user_path)
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :name => "Shane McDonald", :email => "me@shanemcd.com" }
+      end
+
+      # it "should change the user" do
+      #   post :update, :user => @attr
+      #   response.should change(:user)
+      # end
+    end
+  end
 end
